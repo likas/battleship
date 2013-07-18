14d463a203c72907ab60083659a11c2864d0f6d3
k0st1a <kostiamalikov@gmail.com>
2013-07-18 15:00:17 +0700

    add ai_rand_cell.c

---
+++ src/AI/ai_rand_cell.c
@@ -0,0 +1,27 @@
+#include "ai.h"
+
+void ai_rand_cell(int **matr, AI_COORDS *coord)
+{
+	int rand_cell;
+	int count_free_cell = ai_cells_left;
+	rand_cell = (int)(((double) rand() * count_free_cell / (double) RAND_MAX));
+
+//DEBUG_PRINT(printf("rand(0-%d): %d\n",count_free_cell,rand_cell))
+
+	for (int i = 0, count_cell = 0; i < SIZE; ++i) {
+		for (int j = 0; j < SIZE; ++j) {
+			if (matr[i][j] == NONE) {
+				if (count_cell++ == rand_cell) {
+					coord->x = i;
+					coord->y = j;
+
+//DEBUG_PRINT(printf("return_coord.x: %d\n", return_coord.x);printf("return_coord.y: %d\n", return_coord.y);)
+
+					return;
+				}
+			}
+		}
+	}
+	return;
+}
+
