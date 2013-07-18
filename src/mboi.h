b76adf6a857b728ce94f561c231b795754f2e933
cmepiii <cmepiii@sibnet.ru>
2013-07-18 14:15:50 +0700

    Modifiend main header

--- src/mboi.h
+++ src/mboi.h
@@ -1,4 +1,5 @@
 int SIZE = 10;
+#define MSG_LEN 128
 enum _CELL_STATE
 {
 	NONE,
@@ -18,3 +19,33 @@ enum KEY
 	TAB,
 	ESC
 } key;
+
+enum _REQUESTS
+{
+	ACCEPT,
+	DECLINE,
+	HIT,
+	MISS,
+	DESTROYED,
+	YOUWIN,
+	YOULOSE,
+	DISCONNECT,
+	STARTLIST,
+	STORLIST,
+	GAMESTARTED
+} REQ;
+
+typedef struct{
+	int command;
+	char params[128];
+} message;
+
+enum _MSG_TSK
+{
+	NN,//ник
+	RQ,//ответ сервера
+	TT,//сообщение чата
+	AT,//атака
+	HM,//попадание
+	SG//начало игры
+} MSG_TSK;
