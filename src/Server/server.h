7af2564fc2b877c8a1372a416fc9918f49532275
SERGIO MASHKEVICH <lostangel13@yandex.ru>
2013-07-18 15:11:11 +0700

    fix server.h

---
+++ src/Server/server.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<sys/poll.h>
+#include"../mboi.h"
+
+
+extern int game_sockd;
