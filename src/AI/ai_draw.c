fbdd5bba35fc9234d81fbb0130f1d11b4ae74b32
Nikolay Stolbovsky <sknick123@gmail.com>
2013-07-19 12:41:43 +0700

    Changed ai_draw.c

--- src/AI/ai_draw.c
+++ src/AI/ai_draw.c
@@ -13,3 +13,31 @@ void ai_draw(int **field_first, int **field_second)
 		is_first%=2;
 	getch();
 }
+
+int main()
+{
+	int test_area[ SIZE ][ SIZE ] = { 0 };
+	int fire_range[ SIZE ][ SIZE ] = { 0 };
+	COORDS coords;
+
+	ai_init();
+	// init_field
+	ai_set_field( test_area );
+
+	for ( int i = 0; i < SIZE; i++ ) {
+		for ( int j = 0; j < SIZE; j++ ) {
+			fire_range[ i ][ j ] = test_area[ i ][ j ];
+		}
+	}
+	
+	ai_draw( test_area, fire_range );
+
+	ai_rand_cell( test_area, &coords );
+	ai_shoot( coords );
+	ai_clear_variants( test_area );
+	ai_draw( test_area, fire_range );
+
+	ai_uninit();
+
+	return 0;
+}
