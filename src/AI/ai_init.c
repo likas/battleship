c20aed4a552191b01133a518e78ad716c0b5d698
Andrew Teplyashin <thinker@localhost.localdomain>
2013-07-21 13:14:27 +0700

    Fix bug with init ai_last_shot_suc.y

--- src/AI/ai_init.c
+++ src/AI/ai_init.c
@@ -7,7 +7,7 @@ void ai_init()
 	ai_last_shot.x = -1;
 	ai_last_shot.y = -1;
 	ai_last_shot_suc.x = -1;
-	ai_last_shot_suc.x = -1;
+	ai_last_shot_suc.y = -1;
 	ai_direction.dx = 0;
 	ai_direction.dy = 0;
 	got_target = 0;
