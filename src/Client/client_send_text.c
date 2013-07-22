df179e8981d15ea6beb323f8dc3d9c5d6f492011
likas <likas@192.168.3.1>
2013-07-22 11:39:10 +0700

    !!! WARNING !!! version with 'multiple declaration' problem. to fix

--- src/Client/client_send_text.c
+++ src/Client/client_send_text.c
@@ -1,6 +1,6 @@
 #include "client.h"
 
-int client_send_text(int action; char* text){
+int client_send_text(int action, char* text){
 	message	msg; /* struct message{ int command, char* params[128] }
 	/* @ FIELD_START FIELD_END @ */
 	msg.command=action; /* type of message: chat message: NN, TT, SG */
@@ -9,7 +9,7 @@ int client_send_text(int action; char* text){
 	}
 	sscanf(text,"%s",msg.params); /* copying parameters to the message
 								   * structure */
-	if (send(TUNNEL, msg, sizeof(msg),0)!=sizeof(msg)) /* TUNNEL is global 
+	if (send(TUNNEL, (message*)&msg, sizeof(msg),0)!=sizeof(msg)) /* TUNNEL is global 
 														* and attach to socket
 														* we send messages to */
 	{
--- src/Client/coords_itoa.c
+++ src/Client/coords_itoa.c
@@ -1,4 +1,5 @@
-#include "client.h"
+#include "../mboi.h"
+/* #include "client.h" */
 int coords_itoa(char* array, COORDS *xy){
 	xy->x=array[0];
 	xy->y=array[1];
