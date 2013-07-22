bf9822bc4837caddcff2f654f8123120a310e66e
k0st1a <kostiamalikov@gmail.com>
2013-07-22 11:50:56 +0700

    add DIRECTION_WAY in ai_rand_cell.c

--- src/AI/ai_rand_cell.c
+++ src/AI/ai_rand_cell.c
@@ -1,13 +1,13 @@
 #include "ai.h"
 // Returns coordinates of a random free spot on the field
-int ai_rand_cell(int **matr, COORDS *coord, int const change)
+int ai_rand_cell(int **matr, COORDS *coord, DIRECTION_WAY const change)
 {
 	int rand_cell;
 	int count_free_cell = ai_cells_left;
 	rand_cell = (int)(((double) rand() * count_free_cell / (double) RAND_MAX));
 //	printf("rand_cell: %d\n", rand_cell);
 //DEBUG_PRINT(printf("rand(0-%d): %d\n",count_free_cell,rand_cell))
-	if (!change) {
+	if (change == Y_X) {
 		for (int y = 0, count_cell = 0; y < SIZE; ++y) {
 			for (int x = 0; x < SIZE; ++x) {
 				if (matr[y][x] == CELL_NONE) {
@@ -23,7 +23,7 @@ int ai_rand_cell(int **matr, COORDS *coord, int const change)
 				}
 			}
 		}
-	} else {
+	} else if (change == X_Y) {
 		for (int y  = 0, count_cell = 0; y < SIZE; ++y) {
 			for (int x = 0; x < SIZE; ++x) {
 				if (matr[x][y] == CELL_NONE) {
