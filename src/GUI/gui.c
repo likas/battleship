b09e17a467b7f2c4dcf8344fd8b1ba91861e4c69
Andrew Teplyashin <thinker@localhost.localdomain>
2013-07-24 21:16:25 +0700

    Fix holy bugs with more update screen with resize

--- src/GUI/gui.c
+++ src/GUI/gui.c
@@ -21,10 +21,7 @@ if(SIZEFLAG)
 		printf("\033[2J\n");
 		printf("Minumum size is 50x30\n");
 		return;
-	}
-	else SIZEFLAG=0;
-
-	if(!SIZEFLAG)
+	}else
 	{
 		for(i=0;i<10;i++)
   			for(j=0;j<10;j++)
@@ -37,22 +34,11 @@ if(SIZEFLAG)
 		endwin();
 
 		GUICHATLEN=0;
-		initscr();
-		cbreak();
-		noecho();
-		keypad(stdscr, TRUE);
 
 		term_height=stdscr->_maxy;
 		term_width=stdscr->_maxx;
 
 		start_color();
-		init_pair(0,COLOR_BLACK,COLOR_WHITE);
-		init_pair(1,COLOR_WHITE,COLOR_BLACK);
-		init_pair(2,COLOR_GREEN,COLOR_BLUE); //water
-		init_pair(3,COLOR_BLACK,COLOR_WHITE);//ship
-		init_pair(4,COLOR_CYAN,COLOR_RED);//hit
-		init_pair(5,COLOR_MAGENTA,COLOR_YELLOW);//
-
 		keypad(stdscr,TRUE);
 		chat=newwin(term_height/4-2,term_width-1,term_height-term_height/4+2,1);
 
