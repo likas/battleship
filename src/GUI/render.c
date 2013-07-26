46da59b50065076f488e8456e45b677c4345e914
Finiks <finiks@eltex-server.eltex>
2013-07-26 16:21:27 +0700

    net

--- src/GUI/render.c
+++ src/GUI/render.c
@@ -9,22 +9,26 @@ int i,j;
 	if(fl==1) { 
         wbkgdset(op_win[i][j],COLOR_PAIR(op[i][j]));
         wclear(op_win[i][j]);
+	wborder(op_win[i][j],ACS_VLINE,ACS_VLINE,ACS_HLINE,ACS_HLINE,ACS_ULCORNER,ACS_URCORNER,ACS_LLCORNER,ACS_LRCORNER);
         wnoutrefresh(op_win[i][j]);
 	
 	wbkgdset(my_win[i][j],COLOR_PAIR(my[i][j]));
 	wclear(my_win[i][j]);
+	wborder(my_win[i][j],ACS_VLINE,ACS_VLINE,ACS_HLINE,ACS_HLINE,ACS_ULCORNER,ACS_URCORNER,ACS_LLCORNER,ACS_LRCORNER);
 	wnoutrefresh(my_win[i][j]);
 	}
 	
 	if(fl==0) {
 	wbkgdset(my_win[i][j],COLOR_PAIR(my[i][j]));
         wclear(my_win[i][j]);
+	 wborder(my_win[i][j],ACS_VLINE,ACS_VLINE,ACS_HLINE,ACS_HLINE,ACS_ULCORNER,ACS_URCORNER,ACS_LLCORNER,ACS_LRCORNER);
         wnoutrefresh(my_win[i][j]);
 	 }
 	
 	if(fl==2) {
 	wbkgdset(op_win[i][j],COLOR_PAIR(op[i][j]));
         wclear(op_win[i][j]);
+	 wborder(op_win[i][j],ACS_VLINE,ACS_VLINE,ACS_HLINE,ACS_HLINE,ACS_ULCORNER,ACS_URCORNER,ACS_LLCORNER,ACS_LRCORNER);
         wnoutrefresh(op_win[i][j]);
 	}
     }
