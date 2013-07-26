d076149deef853e2eb26cdec0b7c7640bf995243
Finiks <finiks@eltex-server.eltex>
2013-07-26 10:20:01 +0700

    fix start size

--- src/Client/client.c
+++ src/Client/client.c
@@ -14,8 +14,10 @@ int main(int argc, char* argv[]){
 	* when the name is placed, we shall send it to server?
 	* with no idea how... */
 	gui();
-	if(MODEFLAG == QUIT)
+	if(MODEFLAG == QUIT){
+		endgui(3);
 		return;
+	}
 	map_init();
 	if(MODEFLAG >> 1) //Game with ai
 	{
